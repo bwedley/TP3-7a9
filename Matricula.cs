f4b46b4ec1e716416edbd8780da8f62ed709a363
Bruno Wedley <brunowedley27@gmail.com>
2025-11-30 20:11:50 -0300

    Adicionar arquivos de projeto.

---
+++ Matricula.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP3_7a9
+{
+    internal class Matricula
+    {
+        private string _nomeDoAluno;
+        private string _curso;
+        private int _numeroMatricula;
+        private string _situacao; //"Ativa", "Trancada", "Concluída"
+        private string _dataInicial;
+
+        public string NomeDoAluno { get => _nomeDoAluno; set => _nomeDoAluno = value; }
+        public string Curso { get => _curso; set => _curso = value; }
+        public int NumeroMatricula { get => _numeroMatricula; set => _numeroMatricula = value; }
+        public string Situacao { get => _situacao; set => _situacao = value; }
+        public string DataInicial { get => _dataInicial; set => _dataInicial = value; }
+
+        public string Trancar()
+        {
+            Console.WriteLine("A matrícula está trancada");
+            Situacao = "Trancada";
+            return Situacao;
+        }
+        public string Reativar()
+        {
+            Console.WriteLine("A matrícula está reativada");
+            Situacao = "Ativa";
+            return Situacao;
+        }
+
+        public void ExibirInformacoes()
+        {
+            Console.WriteLine("Informações do Aluno: ");
+            Console.WriteLine($"Nome: {NomeDoAluno}");
+            Console.WriteLine($"Curso: {Curso}");
+            Console.WriteLine($"Matricula: {NumeroMatricula}");
+            Console.WriteLine($"Situação atual: {Situacao}");
+            Console.WriteLine($"Data de matricula: {DataInicial}");
+            Console.WriteLine("------------------------------------------------------");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿namespace TP3_7a9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Matricula mat = new Matricula();
+            mat.NomeDoAluno = "Bruno Wedley";
+            mat.Curso = "Análise e Desenvolvimento de Sistemas";
+            mat.NumeroMatricula = 27121993;
+            mat.Situacao = "Ativa";
+            mat.DataInicial = "01/04/2024";
+            Console.WriteLine($"---------------------Informações atuais---------------------");
+            mat.ExibirInformacoes();
+
+            mat.Trancar();
+            Console.WriteLine($"---------------------Informações Trancada---------------------");
+            mat.ExibirInformacoes();
+
+            mat.Reativar();
+            Console.WriteLine($"---------------------Informações Reativada---------------------");
+            mat.ExibirInformacoes();
+
+        }
+    }
+}
